186e054ba0f7655085619558fe41ee8f1caeaa2d
Leon Zagorouiko <student@epicodus.com>
2015-07-22 14:33:25 -0700

    add profile picture.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,7 @@ class CategoriesController < ApplicationController
   load_and_authorize_resource
   def index
     @categories = Category.all
+    @recommendations = Recommendation.all
   end
 
   def show
