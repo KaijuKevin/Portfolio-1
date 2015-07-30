645a0613b1ffc1f0d2a1434a5c151b47532d2606
Leon Zagorouiko <Zagorouiko@gmail.com>
2015-07-30 09:53:15 -0700

    s3 test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/default.jpg"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/default.jpg", :storage => :s3, :bucket => 'zagorouiko', :s3_credentials => S3_CREDENTIALS
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
   has_many :comments
